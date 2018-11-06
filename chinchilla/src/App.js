6d6bca5797730ddff1e5a24840e70e47415bb1c0
xi <napatp.xi@gmail.com>
2018-11-06 17:28:01 +0700

    [Project]

---
+++ chinchilla/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Home from './Page/Home.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Home />
+      </div>
+    );
+  }
+}
+
+export default App;
