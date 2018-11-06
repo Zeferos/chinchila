d49a89c7df7b77be0cc4532fc7dbd6f007a6a469
TonTan <khunanon_43000@hotmail.com>
2018-11-06 19:22:59 +0700

    [update] bg

--- chinchilla/src/Page/WhatWho.js
+++ chinchilla/src/Page/WhatWho.js
@@ -1,18 +1,39 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+// Create the keyframes
+const rotate = keyframes`
+  0% {
+    background-color: #6cd1fd;
+}
 
+  50% {
+    background-color: white;
+}
+100%{
+    background-color: #6cd1fd;
+
+}
+`;
 const Background = styled.div`
     height : 100vh;
     background-color : #6cd1fd;
+    animation: ${rotate} 5s linear infinite;
 `
 
+
+// Here we create a component that will rotate everything we pass in over two seconds
+const Rotate = styled.div`
+//   background-color: red;
+  animation: ${rotate} 2s linear infinite;
+`;
 
 class WhatWho extends Component {
   render() {
     return (
       <div className="App">
       <Background>
-          <img src='.\images\Desktop2\BGs2.png' width="100%"/>
+      <Rotate>&lt; 💅 ">">">">&gt;</Rotate>
+         <img src='.\images\Desktop2\s4-1.png' width="100%" />
           </Background>
       </div>
     );
