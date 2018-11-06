a832ec5240714a5faf07bcd4fe18d889e95574ae
TonTan <khunanon_43000@hotmail.com>
2018-11-06 18:09:57 +0700

    [update] add background color

--- chinchilla/src/Page/WhatWho.js
+++ chinchilla/src/Page/WhatWho.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components'
+
+const Background = styled.div`
+    min-height : 100vh;
+    background-color : #6cd1fd;
+`
+
 
 class WhatWho extends Component {
   render() {
     return (
       <div className="App">
-      <p>WhatWho</p>
+      <Background>
+          <p>WhatWho</p>
+          </Background>
       </div>
     );
   }
 }
 
-export default WhatWho;
+export default WhatWho; 
