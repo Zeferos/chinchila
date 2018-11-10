983f0ad1a75d7587164ce092e8eed0f8974ef567
xi <napatp.xi@gmail.com>
2018-11-11 01:17:51 +0700

    [update]Contact

--- chinchilla/src/Page/Contact.js
+++ chinchilla/src/Page/Contact.js
@@ -11,12 +11,62 @@ font-family: 'Mali SemiBold';
 font-size : 70px;
 color: white;
 `
+const Line1 = styled.img`
+ position: absolute;
+ top: 295em;
+ left: 25em;
+ width: 0.3%;
+`
+const Line2 = styled.img`
+ position: absolute;
+ top: 295em;
+ left: 69em;
+ width: 0.3%;
+`
+const Phone = styled.img`
+ position: absolute;
+ top: 300em;
+ left: 14em;
+ width: 5%;
+`
+const Facebook = styled.img`
+ position: absolute;
+ top: 300em;
+ left: 31.5em;
+ width: 5%;
+`
+const Instragram = styled.img`
+ position: absolute;
+ top: 300em;
+ left: 45.5em;
+ width: 5%;
+`
+const LineApp = styled.img`
+ position: absolute;
+ top: 300em;
+ left: 59em;
+ width: 5%;
+`
+const Mail = styled.img`
+ position: absolute;
+ top: 300em;
+ left: 75em;
+ width: 5%;
+`
 class Contact extends Component {
     render() {
         return (
             <div>
                 <Background>
                     <Contacttxt><a>Contact</a></Contacttxt>
+                    <Phone src='.\images\Contact\phone-call.png' />
+                    <Line1 src='.\images\Contact\stline.png' />
+                    <Line2 src='.\images\Contact\stline.png' />
+                    <Facebook src='.\images\Contact\facebook.png' />
+                    <Instragram src='.\images\Contact\instagram.png' />
+                    <LineApp src='.\images\Contact\line.png' />
+                    <Mail src='.\images\Contact\mail-black-envelope-symbol.png' />
+
                 </Background>
             </div>
         );
--- chinchilla/src/Page/FAQ.js
+++ chinchilla/src/Page/FAQ.js
@@ -8,12 +8,38 @@ const Background = styled.div`
 const FAQBG = styled.img`
  width: 100%;
 `
+const Leaves = styled.img`
+position: absolute;
+left: 5em;
+top: 215em;
+width: 25%;
+`
+const FAQs = styled.div`
+position: absolute;
+top: 445%;
+left: 45%;
+font-family: 'Mali SemiBold';
+font-size : 90px;
+color: black;
+`
+const FAQsth = styled.div`
+position: absolute;
+top: 461%;
+left: 55%;
+font-family: 'Mali';
+font-size : 35px;
+color: black;
+`
 class FAQ extends Component {
     render() {
         return (
             <div>
                 <Background>
                     <FAQBG src='.\images\FAQ\bg-01.png' />
+                    <Leaves src='.\images\FAQ\leaves.png' />
+                    <FAQs><a>FAQs</a></FAQs>
+                    <FAQsth><a>คำถามที่พบบ่อย?</a></FAQsth>
+
                 </Background>
             </div>
         );
