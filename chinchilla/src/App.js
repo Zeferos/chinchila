cb1f63891eecd30956cd36e6f3f69719eec68701
TonTan <khunanon_43000@hotmail.com>
2018-11-11 20:48:19 +0700

    add navbar

--- chinchilla/src/App.js
+++ chinchilla/src/App.js
@@ -11,6 +11,12 @@ class App extends Component {
     return (
       <div className="App">
         {/* <Navbar /> */}
+        <ul>
+          <li><a class="active" href="#home">Home</a></li>
+          <li><a href="#news">News</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
         <Home />
         <WhatWho />
         <WhereWhen />
